baeb797a1754e5e3d4827020ff8b8c9fe2656ad7
Nabz <bnnabiswa@gmail.com>
2020-05-25 02:10:10 +0300

    add image model methods

--- piczone_app/models.py
+++ piczone_app/models.py
@@ -7,3 +7,30 @@ class Image(models.Model):
     location = models.ForeignKey('Location',on_delete=models.CASCADE)
     category = models.ForeignKey('Category',on_delete=models.CASCADE)
     upload_date= models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        return self.save() 
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        return cls.objects.filter(id=id).all()
+
+    @classmethod
+    def search_image_by_category(cls, search_term):
+        return cls.objects.filter(category__icontains = search_term)
+ 
+    @classmethod
+    def filter_by_location(cls, location):
+        return cls.objects.filter(location__name=location).all()
+        
+    @classmethod
+    def update_image(cls, id, new_img):
+        cls.objects.filter(id=id).update(image=new_img)
+
