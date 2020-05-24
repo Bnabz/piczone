dba5cf36bb308ecea676a23a511480a217219943
Nabz <bnnabiswa@gmail.com>
2020-05-25 01:04:04 +0300

    create image model

--- piczone_app/models.py
+++ piczone_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length =60)
+    description = models.CharField(max_length =240)
+    location = models.ForeignKey('Location',on_delete=models.CASCADE)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    upload_date= models.DateTimeField(auto_now_add=True)
