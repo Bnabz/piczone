e70f7db1d714fcfb7a80b25b9679a86597f1e2a9
Nabz <bnnabiswa@gmail.com>
2020-05-25 05:42:05 +0300

    passed Category model test

--- piczone_app/tests.py
+++ piczone_app/tests.py
@@ -49,6 +49,25 @@ class TestImage(TestCase):
         Location.objects.all().delete()
         Category.objects.all().delete()
 
+
+class TestCategory(TestCase):
+    def setUp(self):
+        self.category = Category(name='cars')
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+
     
 
 
