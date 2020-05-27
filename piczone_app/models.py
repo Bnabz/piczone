656f07a3245cbb3feb0d5225e08e33a6e3473037
Nabz <bnnabiswa@gmail.com>
2020-05-27 07:34:25 +0300

    include image upload field

--- piczone_app/models.py
+++ piczone_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Image(models.Model):
-    image = models.ImageField()
+    image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length =60)
     description = models.CharField(max_length =240)
     location = models.ForeignKey('Location',on_delete=models.CASCADE)
