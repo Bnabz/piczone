9ca48c0ea008a7a5c961c5d8b19f7a23e37bf706
Nabz <bnnabiswa@gmail.com>
2020-05-27 05:57:11 +0300

    update heroku configs

--- piczone_project/settings.py
+++ piczone_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '8k9$2)#$mcem^u*(rg5@xcbi-j@wkjywdu*ek(2sbdht4ri&v0'
+SECRET_KEY = '8k9$2)#$mcem^u*(rg5@xcbi-j@wkjywdu*ek(2sbdht4ri&v0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
